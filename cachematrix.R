685dc09e80b7ec533b05323982481922484af821
dotjabber <mkowalski@wilddogsoftware.pl>
2015-11-23 00:09:45 +0100

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+## Maciej Kowalski
+## Coursera // R Programming // Assignment 2
+
+## General idea is exactly the same as in assignment - there is a list 
+## created, containing:
+## - getter and setter to given matrix
+## - getter and setter to inversed matrix
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   
@@ -20,6 +27,18 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
+## First of all, we need a matrix, we can create it by calling 
+## makeCacheMatrix() function. 
+## Such created matrix is then given to this method, if it finds
+## i <- x$getInv(), then your inverse matrix is taken from cache
+## otherwise, it will be computed.
+##
+## usage:
+## m1 <- matrix(sample(25), ncol = 5, nrow = 5) # make regular matrix
+## e1 <- makeCacheMatrix(m1) # make cached matrix
+## i1 <- cacheSolve(e1); # if you run this for first time, it will spit no message
+## i1 <- cacheSolve(e1); # if you'll do it again, it will give you proper message :)
+## et voila :)
 cacheSolve <- function(x, ...) {
   i <- x$getInv()
   
